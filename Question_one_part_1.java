d74f1326f69d7b9822614aeaa8b1ea5416255184
AvaKathrynShaw <aks174@scarletmail.rutgers.edu>
2015-10-29 15:55:52 -0400

    First Commit

---
+++ Question_one_part_1.java
@@ -0,0 +1,29 @@
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.filter.*;
+import java.util.Random;
+
+
+public class Question_one implements PlugInFilter {
+	ImagePlus imp;
+
+	public int setup(String arg, ImagePlus imp) {
+		this.imp = imp;
+		imp.draw();
+		imp.getProcessor();
+		return DOES_ALL;
+	}
+
+	public void run(ImageProcessor ip) {
+	
+	Random random = new Random();
+	int abc = ramdon.nextint(255);
+
+	ip.add(abc);
+
+	
+	}
+
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+These are some silly little filters for Rutgers Multi-Media Homework. Enjoy!
